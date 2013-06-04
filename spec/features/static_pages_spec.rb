5e5f4cd2e7266c077ad1815a38b6a850dce0ac2b
John Camelon <john.camelon@gmail.com>
2013-06-04 09:40:23 -0400

    Fixed SASS, headers, used routes

--- spec/features/static_pages_spec.rb
+++ spec/features/static_pages_spec.rb
@@ -2,38 +2,35 @@ require 'spec_helper'
 
 describe "StaticPages" do
   describe "Home page" do
-    it "should have the content 'Sample App'" do
-      visit '/static_pages/home'
-      page.should have_content('Sample App')
-    end
 
-    it "should have the right title" do
-      visit '/static_pages/home'
-      page.should have_title("ROR Sample App")
-    end
+    before { visit root_path }
+    subject{ page }
+
+    it { should have_selector('h1', 'Sample App') }
+    it { should have_title(full_title(''))}
+
   end
 
   describe "Help page" do
-    it "should have the content 'Sample App'" do
-      visit '/static_pages/help'
-      page.should have_content('Help')
-    end
+
+    before { visit help_path }
+    subject{ page }
+
+    it { should have_selector('h1', 'Help') }
+
   end
 
   describe "About page" do
-    it "should have the content 'About Us'" do
-      visit '/static_pages/about'
-      page.should have_content('About Us')
-    end
+    before { visit about_path }
+    subject{ page }
+
+    it { should have_selector('h1', 'About') }
   end                                                                                                                                                                              end
 
   describe "Contact page" do
-    it "should have the content 'Contact'" do
-      visit '/static_pages/contact'
-      page.should have_content('Contact')
-    end
-    it "should have the title 'Contact'" do
-      visit '/static_pages/contact'
-      page.should have_title("ROR Sample App | Contact")
-    end
+    before { visit contact_path }
+    subject{ page }
+
+    it { should have_selector('h1', 'Contact') }
+    it { should have_title(full_title('Contact')) }
   end
